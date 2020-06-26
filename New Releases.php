1c4de4bb86a4d4c3a81d6678897d52e040c36e0d
TRBoom <schriever@gmail.com>
2020-06-26 09:13:57 -0400

    Pages and dates

---
+++ New Releases.php
@@ -0,0 +1,45 @@
+<?php session_start(); ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Spamazon</title>
+<link rel="stylesheet" type="text/css" href="css/spamstyle.css">
+</head>
+
+<body>
+<?php
+    
+include_once('Header.php');
+include('db_key.php');
+
+?>
+
+<?php
+
+$today=date("Y-m-d");
+$week=date('Y-m-d', strtotime('-90 days'));
+$query = mysqli_query($dbconnect, "SELECT book.title, book.price, book.publishDate, book.bookNo,author.authorNo, author.authorName FROM book 
+										INNER JOIN wroteBy ON wroteBy.bookNo=book.bookNo
+										INNER JOIN author ON author.authorNo=wroteBy.authorNo
+										WHERE publishDate <'$today' AND publishDate >'$week'")
+or die (mysqli_error($dbconnect));
+
+echo "<div class='grid-container' border='1' align='center'>";
+while ($row = mysqli_fetch_array($query)) {
+  echo
+   "<div class='grid-item'>
+   <table align='center'>
+   <tr><td><a href='book.php?ID={$row['bookNo']}'>{$row['title']}</td></tr>
+   <tr><td>by</td></tr>
+   <tr><td><a href='bio.php?ID={$row['authorNo']}'>{$row['authorName']}</td></tr>
+   <tr><td>\${$row['price']}</td></tr>
+	</table>
+   </div>";
+
+}
+
+
+?>
+</div>
+</body>
+</html>
--- book.php
+++ book.php
@@ -11,45 +11,31 @@
 include_once('Header.php');
 include('db_key.php');
 
-$query = mysqli_query($dbconnect, "SELECT book.title FROM book
-										WHERE bookNo='".$_GET['ID']."' ")
+$query = mysqli_query($dbconnect, "SELECT book.title, book.price,book.bookNo,book.publishDate, author.authorNo, author.authorName FROM book 
+										INNER JOIN wroteBy ON wroteBy.bookNo=book.bookNo
+										INNER JOIN author ON author.authorNo=wroteBy.authorNo
+										WHERE book.bookNo='".$_GET['ID']."' ")
    or die (mysqli_error($dbconnect));
    
 $bookInfo=mysqli_fetch_array($query);
 
 echo "<h1>{$bookInfo['title']}</h1>";
-
-?>
-
-
-<table border="1" align="center">
-<tr>
-  <td>Book Title</td>
-  <td>Author</td>
-  <td>Price</td>
-  <td></td>
-</tr>
-
-<?php
-
-$query = mysqli_query($dbconnect, "SELECT book.title, book.price,book.bookNo,author.authorNo, author.authorName FROM book 
-										INNER JOIN wroteBy ON wroteBy.bookNo=book.bookNo
-										INNER JOIN author ON author.authorNo=wroteBy.authorNo
-										WHERE book.bookNo='".$_GET['ID']."' ")
-   or die (mysqli_error($dbconnect));
-
-while ($row = mysqli_fetch_array($query)) {
-  echo
-   "<tr>
-    <td><a href='book.php?ID={$row['bookNo']}'>{$row['title']}</td>
-    <td><a href='bio.php?ID={$row['authorNo']}'>{$row['authorName']}</td>
-    <td>\${$row['price']}</td>
-	<td><a href='cart.php?ID={$row['bookNo']}'><h2 align='center'>Add to Cart</h2></td>
-   </tr>\n";
-
+echo "<h1>by</h1>";
+echo "<h1>{$bookInfo['authorName']}</h1>";
+echo "<h1>\${$bookInfo['price']}</h1>";
+$today=date("Y-m-d");
+$passed=time() - strtotime($bookInfo['publishDate']);
+echo"$passed";
+if ($today>$bookInfo['publishDate']){
+	echo"<a href='cart.php?ID={$bookInfo['bookNo']}'><h2>Add to Cart</h2></a>";
+	echo "<h4>Published on {$bookInfo['publishDate']}</h4>";
 }
-echo"</table>";
+else{
+	echo "<h4>Release date {$bookInfo['publishDate']}</h4>";
+}
+
 ?>
+
 
 </body>
 </html>
